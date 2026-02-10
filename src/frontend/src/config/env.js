f5cce2020dd4bad2e33bbe2da92fb76119f66196
nimrodlahav <128465134+nimrodlahav@users.noreply.github.com>
2026-02-10 12:25:19 +0200

    Update env.js

--- src/frontend/src/config/env.js
+++ src/frontend/src/config/env.js
@@ -4,7 +4,7 @@ const localIP = Constants.expoConfig?.hostUri?.split(":")[0] || "192.168.1.240";
 
 export const ENV = {
   dev: {
-    API_URL: `http://${localIP}:3000`, // <-- matches your backend
+    API_URL: `http://${localIP}:3000`,
   },
   prod: {
     API_URL: "https://your-production-server.com",
